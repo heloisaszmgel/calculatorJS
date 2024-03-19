91ca56843e9b397366137599b4c4bf516d4b6bc2
heloisaszmgel <heloszmgel@gmail.com>
2024-03-19 11:25:51 -0400

    added comments in the script file

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 function appendToDisplay(value) {
     document.getElementById('display').value += value;
-  }
-  
-  function clearDisplay() {
-    document.getElementById('display').value = '';
-  }
-  
-  function calculate() {
-    try {
-      const result = eval(document.getElementById('display').value);
-      document.getElementById('display').value = result;
-    } catch (error) {
-      document.getElementById('display').value = 'Error';
     }
-  }
-  
+// responsible for appending the clicked button's value to the display/input field.
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+    }
+
+// clears the display/input field. it sets the new value to an empty string.
+    
+function calculate() {
+    try {
+        const result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
+// responsible for evaluating the expression entered in the display/input field and displaying the result.
